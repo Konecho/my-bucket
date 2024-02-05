d88f8f242724e39e395e4a3f090ad17adfab23dd
konecho <13230837+konecho@user.noreply.gitee.com>
2024-02-05 11:42:52 +0800

    update

--- check_duplicate.py
+++ check_duplicate.py
@@ -1,15 +1,32 @@
 from pathlib import Path
 import json
-Path()
-this_bucket_name=Path().cwd().name
-print(this_bucket_name)
-other_bucket_list=[p for p in Path('..').glob('*') if p.name!=this_bucket_name]
-print(other_bucket_list)
-for app in Path('./bucket').glob('*.json'):
+
+this_bucket_name = Path().cwd().name
+
+other_bucket_list = [p for p in Path('..').glob(
+    '*') if p.name != this_bucket_name]
+print(f"\n{this_bucket_name} <=> {other_bucket_list}\n")
+
+
+def is_duplicate(app: Path):
     for bucket in other_bucket_list:
         if (bucket/'bucket'/app.name).exists():
-            this_ver=json.load(app.open(encoding='utf-8'))['version']
-            other_ver=json.load((bucket/'bucket'/app.name).open(encoding='utf-8'))['version']
-            print(f'duplicate app {app.stem}[{this_ver}] <=> bucket {bucket.name}[{other_ver}]')
+            this_ver = json.load(app.open(encoding='utf-8'))['version']
+            other_ver = json.load(
+                (bucket/'bucket'/app.name).open(encoding='utf-8'))['version']
+            print(
+                f'[duplicate] {app.stem} [{this_ver}] <=> [{other_ver}] in <{bucket.name}>')
+            return True
+    return False
 
 
+def should_add_checkver(app: Path):
+    f = json.load(app.open(encoding='utf-8'))
+    assert isinstance(f, dict)
+    return not ('checkver' in f.keys()) and (not (f['url'].startswith('https://link.jscdn.cn/' or f['url'].startswith('https://dlink.host/1drv/'))))
+
+
+for app in Path('./bucket').glob('*.json'):
+    is_duplicate(app)
+    if should_add_checkver(app):
+        print(f'[checkver?] {app.stem}')

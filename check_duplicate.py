80eaf0206d7f81e8dd6aaef0d90b8738967ee5f0
请假王 <konecho@outlook.com>
2025-01-16 12:01:39 +0800

    update

--- check_duplicate.py
+++ check_duplicate.py
@@ -2,42 +2,50 @@ import json
 from logging import info, warning
 from pathlib import Path
 
-this_bucket_name = Path().cwd().name
+this_bucket = Path().cwd()
 
-other_bucket_list = [
-    p for p in Path("..").glob("*") if p.name != this_bucket_name and p.is_dir()
+other_buckets = [
+    p for p in Path("..").glob("*") if p.name != this_bucket.name and p.is_dir()
 ]
 
-print(f"\n{this_bucket_name} <=> {list(map(lambda b:b.stem,other_bucket_list))}\n")
+print(f"\n{this_bucket.name} <=> {list(map(lambda b:b.name,other_buckets))}\n")
+
+manifests = {
+    bucket: list((bucket / "bucket").glob("*.json")) for bucket in other_buckets
+}
 
 
-def is_duplicate(app: Path):
-    for bucket in other_bucket_list:
-        if (bucket / "bucket" / app.name).exists():
-            this_ver = json.load(app.open(encoding="utf-8"))["version"]
-            other_ver = json.load(
-                (bucket / "bucket" / app.name).open(encoding="utf-8")
-            )["version"]
-            print(
-                f"[duplicate] {app.stem} [{this_ver}] <=> [{other_ver}] in <{bucket.name}>"
-            )
-            return True
+def load_manifest(app: Path):
+    f = json.load(app.open(encoding="utf-8"))
+    assert isinstance(f, dict)
+    return f
+
+
+def is_duplicate(this_app: Path):
+    for bucket, apps in manifests.items():
+        for app in apps:
+            if this_app.stem.replace("-", "") == app.stem.replace("-", ""):
+                this_ver = load_manifest(this_app)["version"]
+                other_ver = load_manifest(app)["version"]
+                print(
+                    f"[duplicate] {this_app.stem} [{this_ver}] <=> [{other_ver}] {app.stem} in <{bucket.name}>"
+                )
+                return True
     return False
 
 
 def should_add_checkver(app: Path):
-    f = json.load(app.open(encoding="utf-8"))
-    assert isinstance(f, dict)
-    return not ("checkver" in f.keys()) and (
-        not (
-            f["url"].startswith("https://link.jscdn.cn/")
-            or f["url"].startswith("https://dlink.host/1drv/")
-        )
+    f = load_manifest(app)
+    return not (
+        "checkver" in f.keys()
+        or f["url"].startswith("https://link.jscdn.cn/")
+        or f["url"].startswith("https://dlink.host/1drv/")
     )
 
 
-for app in Path("./bucket").glob("*.json"):
+for app in (this_bucket / "bucket").glob("*.json"):
     info(app)
     is_duplicate(app)
     if should_add_checkver(app):
+        warning(f"[checkver?] {app.stem}")
         warning(f"[checkver?] {app.stem}")
